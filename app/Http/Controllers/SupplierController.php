382913f36e397bd824991abc2924c5edaa193b7d
EmmaF535 <128007246+EmmaF535@users.noreply.github.com>
2024-05-12 22:49:12 -0600

    second run
    mas o menos queda suppliers, hay dudas

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -9,12 +9,88 @@ class SupplierController extends Controller
 {
     public function index()
     {
-    
-    $suppliers = Supplier::select('id','company_name','address','phone')
-    ->orderby('id','asc')
-    ->get();
-    
-    return view('suppliers.index',compact('suppliers'));
+        //$suppliers = Supplier::todos_los_suppliers();
+        $suppliers = Supplier::select('id','company_name','address','phone')
+            ->orderby('id','asc')
+            ->get();
+        
+        return view('suppliers.index',compact('suppliers'));
 
+    }
+
+    public function create()
+    {
+        return view('suppliers.create');    
+    }
+
+    public function store(Request $request)
+    {
+        Supplier::create([
+            'company_name'=>$request->company_name,
+            'address'=>$request->address,
+            'phone'=>$request->phone
+        ]);
+
+        return redirect()->route('suppliers.index')
+            ->with('success', 'Supplier creado exitosamente');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('suppliers.edit')
+            ->with('suppliers', Supplier::supplier_por_id($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $suppliers = Supplier::supplier_por_id($id);
+
+        $suppliers->update([
+            'company_name' =>  $request->company_name,
+            'address'   => $request->address,
+            'phone'   =>  $request->phone
+        ]);
+
+        return redirect()->route('suppliers.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $suppliers = Supplier::supplier_por_id($id);
+
+        $note->delete();
+        
+
+        return redirect()->route('suppliers.index');
     }
 }
--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -34,10 +34,11 @@
             <div class="col-6"> <!-- Utilizamos una columna de 6 para alinear el título a la izquierda -->
                 <h1 class="bg-primary text-white text-center py-4">Suppliers</h1>
             </div>
-            <div class="col-4 text-end"> <!-- Utilizamos una columna de 6 para alinear el botón a la derecha -->
-                <button type="button" class="btn btn-success mt-4 me-3">Add Supplier</button>
+        <div class="col-4 text-end"> <!-- Utilizamos una columna de 6 para alinear el botón a la derecha -->
+                <a href="{{ route('suppliers.create') }}" class="btn btn-success">Add supplier + </a>
             </div>
         </div>
+        
         <div class="row">
             <div class="col">
                 <div class="table-responsive"> <!-- Tabla responsiva-->
@@ -61,8 +62,13 @@
                                 <td> {{$item->address}}</td>
                                 <td> {{$item->phone}}</td>
                                 <td>
-                                    <button type="submit" class="btn btn-primary">Update</button>
-                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                    <a href="{{ route('suppliers.edit', $item->id) }}" class="btn btn-info">Update</a>
+                                    <form action="{{ route('suppliers.destroy', $item->id) }}" method="post">
+                                        @csrf
+                                        @method('delete')
+
+                                        <input type="submit" value="Eliminar" class="btn btn-danger" onclick="return confirm('¿Estás seguro que quieres eliminarlo?')">
+                                    </form>
                                 </td>
                             </tr>
 
